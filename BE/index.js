6d770b96064522e7adf98993656d95def130ed25
nghianvhe180712@fpt.edu.vn <nghianvhe180712@fpt.edu.vn>
2026-03-16 20:14:34 +0700

    fix index

--- BE/index.js
+++ BE/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const path = require('path');
+const nodePath = require('path');
 const swaggerUi = require('swagger-ui-express');
 const getServerUrl = require('./utils/getServerUrl');
 const userRoutes = require("./routes/userRoutes");
@@ -35,7 +35,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(nodePath.join(__dirname, 'public')));
 
 // Swagger UI - ĐẶT TRƯỚC các routes khác
 const swaggerOptions = {
@@ -51,7 +51,7 @@ app.get('/swagger.json', (req, res) => {
 
 // Routes
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'render-deploy.html'));
+  res.sendFile(nodePath.join(__dirname, 'public', 'render-deploy.html'));
 });
 
 app.get('/api-info', (req, res) => {
